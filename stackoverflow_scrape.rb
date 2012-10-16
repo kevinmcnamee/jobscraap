44d98a0f931f236e4609afef3e0ddb3ae7a81e83
Kevin <mcnameekm@gmail.com>
2012-10-16 17:37:55 -0400

    did a bunch of work and I hope this works out

--- stackoverflow_scrape.rb
+++ stackoverflow_scrape.rb
@@ -8,15 +8,51 @@ print 'what type of job would you like to search: '
 search_term = gets.chomp
 search_term = search_term.split.join("+")
 
-print 'how many miles would you like to search? '
+print 'how many miles from NYC would you like to search? '
 range = gets.chomp
 
-#create index for website
-index_page = Nokogiri::HTML(open("http://careers.stackoverflow.com"))
+index_url = "http://careers.stackoverflow.com"
+search_url = "/jobs?searchTerm=#{search_term}&location=new+york&range=#{range}"
+search_results = Nokogiri::HTML(open("#{index_url}#{search_url}"))
 
-#open search page
-search_page = Nokogiri::HTML(open("http://careers.stackoverflow.com/jobs?searchTerm=#{search_term}&location=new+york&range=#{range}"))
+#get all of the result pages if multiple pages
+search_pages = []
+search_results.css("div.pagination a").map do |link|
+	search_pages << index_url + link["href"]
+end
 
-#open job listings page
-listings_page = Nokogiri::HTML(open())
+search_pages = search_pages.uniq!
 
+job_links = []
+
+search_pages.each do |page|
+	doc = Nokogiri::HTML(open(page))
+	doc.css("a.title").map do |link|
+		job_links << index_url + link['href']
+	end
+end
+
+job_links.each do |link|
+	listings_page = Nokogiri::HTML(open(link.to_s))
+
+	#company
+	company_name = listings_page.css("div#hed a.employer").text
+
+	#job titles
+	job_title = listings_page.css("div#hed h1#title a.title").text
+
+	#locations
+	locations = listings_page.css("div#hed span.location").text
+
+	#job description
+	job_content = listings_page.css("div.jobdetail div.description")
+
+	description = job_content[0].text.strip
+	if job_content.length >= 2
+		requirements = job_content[1].text
+	elsif job_content.length > 2
+		about = job_content[2].text
+	end
+
+
+end	
